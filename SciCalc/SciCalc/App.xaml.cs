aed722174926f11e11082fd857613bdc90d30ad8
MarioRaid99 <33650221+MarioRaid99@users.noreply.github.com>
2025-09-15 12:20:59 +0300

    Initial commit

---
+++ SciCalc/SciCalc/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using SciCalc.Views;
+
+namespace SciCalc
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CalculatorPage();
+        }
+
+
+        //protected override Window CreateWindow(IActivationState? activationState)
+        //{
+        //    return new Window(new AppShell());
+        //}
+    }
+}
